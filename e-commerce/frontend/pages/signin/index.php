a4e685c5d2784e4a28c20736b5c8674bbe57eb10
darkeny <darkenpaulo00@gmail.com>
2023-04-08 23:07:20 +0200

    creating authenticate file

--- e-commerce/frontend/pages/signin/index.php
+++ e-commerce/frontend/pages/signin/index.php
@@ -15,21 +15,28 @@
 
 <body>
     <div class="m-3 float-end">
-        <a href="new.php"><div class="btn btn-primary">Sign-up</div></a>
+        <a href="new.php">
+            <div class="btn btn-primary">Sign-up</div>
+        </a>
     </div>
     <div class="container">
         <div class="container-login-small content-center w-100 ">
             <div class="shadow p-5 brd-15">
+                <?php
+                if (isset($_SESSION['error-first'])) {
+                    echo $_SESSION['error-first'];
+
+                    unset($_SESSION['error-first']);
+                }
+                ?>
                 <h1 class="fw-bold text-center">Sign-in</h1>
                 <form action="#" method="post" class="needs-validation" novalidate>
                     <div class="form-floating">
-                        <input type="text" id="name" class="form-control hidden-focus" required
-                            placeholder="Insira seu nome">
+                        <input type="text" id="name" class="form-control hidden-focus" required placeholder="Insira seu nome">
                         <label for="name">Insira o seu nome</label>
                     </div>
                     <div class="form-floating my-2">
-                        <input type="password" id="password" class="form-control hidden-focus" required
-                            placeholder="Insira seu nome">
+                        <input type="password" id="password" class="form-control hidden-focus" required placeholder="Insira seu nome">
                         <label for="name">Insira a sua senha</label>
                     </div>
                     <div class="m-3  text-center">

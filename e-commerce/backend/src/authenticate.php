a2d5ec94336248481812bac6e2e195ab39288291
darkeny <darkenpaulo00@gmail.com>
2023-04-08 23:15:22 +0200

    implements authenticate file

---
+++ e-commerce/backend/src/authenticate.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+include("conexao.php");
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
+    $senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
+
+    function auth($conn, $email, $senha)
+    {
+        $sql = "SELECT * FROM User WHERE email = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue(1, $email);
+        try {
+            $stmt->execute();
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+            if ($user &&password_verify($senha, $user['senha'])) {
+                return $user;
+            } else {
+                return false;
+            }
+        } catch (PDOException $e) {
+            echo "Erro no banco de dados: " . $e->getMessage();
+        }
+    }
+
+    $auth = auth($conn, $email, $senha);
+
+    if ($auth) {
+        $_SESSION['values'] = $auth;
+        header('Location: ../../frontend/pages/signin/new.php');
+    } else {
+        $_SESSION['error-first'] = "<div class='alert alert-danger' role='alert'>Usuário ou senha incorreto</div>";
+        header('Location: ../../frontend/pages/signin/index.php');
+    }
+} else {
+    header('Location: ../../frontend/pages/signin/index.php');
+}

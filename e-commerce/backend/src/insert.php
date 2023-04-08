7e8a28155a71569536dd3972769737d667147edd
darkeny <darkenpaulo00@gmail.com>
2023-04-08 23:02:36 +0200

    adding alert to new.php form

--- e-commerce/backend/src/insert.php
+++ e-commerce/backend/src/insert.php
@@ -14,19 +14,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $stmt->execute();
         $count = $stmt->fetchColumn();
         if ($count > 0) {
-            $_SESSION['error'] = "Email já existe no banco de dados.";
+            $_SESSION['error'] = "<div class='text-center alert alert-danger' role='alert'>Email já existe no banco de dados.</div>";
             header('Location:  ../../frontend/pages/signin/new.php');
-        }else{
+        } else {
             $encryptPass = password_hash($userArgs['senha'], PASSWORD_BCRYPT);
             // Preparar a declaração SQL para inserir o registro
             $sql = "INSERT INTO User (nome, email, senha) VALUES (?,?,?)";
             $stmt = $conn->prepare($sql);
-    
+
             // Vincular os parâmetros
             $stmt->bindValue(1, $userArgs['nome']);
             $stmt->bindValue(2, $userArgs['email']);
             $stmt->bindValue(3, $encryptPass);
-    
+
             // Executar a declaração SQL
             try {
                 $stmt->execute();
@@ -39,5 +39,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } catch (PDOException $e) {
         echo "Erro com banco de dados" . $e->getMessage();
     }
-
 }

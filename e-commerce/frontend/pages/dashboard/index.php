045f068b3302e7dd39f702be9488333cd2733022
darkeny <darkenpaulo00@gmail.com>
2023-04-13 23:27:23 +0200

    implements home ppage

--- e-commerce/frontend/pages/dashboard/index.php
+++ e-commerce/frontend/pages/dashboard/index.php
@@ -32,7 +32,7 @@ if (!isset($_SESSION['values'])) {
                 <div class="collapse navbar-collapse" id="navbarText">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="./index.php">Início</a>
+                            <a class="nav-link active" aria-current="page" href="../home/index.php">Início</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="../dashboard/index.php">Painel de Controle</a>
@@ -49,7 +49,7 @@ if (!isset($_SESSION['values'])) {
                     </ul>
                     <span class="navbar-text">
                         <a href="index.php">
-                            <img src="../../img/happy-girl.jpg" class="small-logo">
+                            <img src="../../img/logo/perfil.jpg" class="small-logo">
                         </a>
                     </span>
                 </div>

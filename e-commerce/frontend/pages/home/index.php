35d95b51e5fbe5bc6c576f6b8dd1ac4941e64b3f
darkeny <darkenpaulo00@gmail.com>
2023-04-14 17:55:42 +0200

    adding animate lib in the localhost

--- e-commerce/frontend/pages/home/index.php
+++ e-commerce/frontend/pages/home/index.php
@@ -6,9 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="http://localhost/public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://localhost/public/css/animate.css/animate.min.css">
     <link rel="stylesheet" href="../../styles/css/globals.css">
     <script src="http://localhost/public/js/bootstrap.min.js" async defer></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" /> -->
 
     <title>Malanga Store</title>
 </head>

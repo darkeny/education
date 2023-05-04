1a5ffe8bbbf6b4826b7c710b5f0452f2f489df4b
darkeny <darkenpaulo00@gmail.com>
2023-05-04 21:11:51 +0200

    upgrade: create a ecommerce stage

---
+++ e-commerce/frontend/pages/products/index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="http://localhost/public/css/bootstrap.min.css">
+	<link rel="stylesheet" href="http://localhost/public/css/animate.css/animate.min.css">
+	<link rel="stylesheet" href="../../styles/css/globals.css">
+	<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" /> -->
+	<script src="http://localhost/public/js/bootstrap.min.js" async defer></script>
+
+
+
+	<title>Malanga Store</title>
+</head>
+
+<body>
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-light">
+			<div class="container">
+				<a class="navbar-brand text-muted " href="../home/index.php">
+					<img src="../../img/logo/logo.png" class="animate__animated animate__pulse" width="130px">
+				</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarText">
+					<ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
+						<li class="nav-item">
+							<a class="nav-link active" aria-current="page" href="../home/index.php">Início</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="../dashboard/index.php">Painel de Controle</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Suporte</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Productos</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Sobre nós</a>
+						</li>
+					</ul>
+					<span class="navbar-text">
+						<a href="./new.php">
+							<div class="btn btn-primary">Sign-up</div>
+						</a>
+					</span>
+				</div>
+			</div>
+		</nav>
+	</header>
+	<section class="container my-5">
+		<div class="row row-cols-1 row-cols-md-3 g-4">
+			<!-- Card 1 -->
+			<div class="col">
+				<div class="card h-100">
+					<img src="https://via.placeholder.com/300x200.png?text=Produto+1" class="card-img-top" alt="Imagem do Produto 1">
+					<div class="card-body">
+						<h5 class="card-title">Produto 1</h5>
+						<p class="card-text">Descrição breve do produto 1.</p>
+					</div>
+					<div class="card-footer">
+						<small class="text-muted">$19.99</small>
+					</div>
+				</div>
+			</div>
+			<!-- Card 2 -->
+			<div class="col">
+				<div class="card h-100">
+					<img src="https://via.placeholder.com/300x200.png?text=Produto+2" class="card-img-top" alt="Imagem do Produto 2">
+					<div class="card-body">
+						<h5 class="card-title">Produto 2</h5>
+						<p class="card-text">Descrição breve do produto 2.</p>
+					</div>
+					<div class="card-footer">
+						<small class="text-muted">$29.99</small>
+					</div>
+				</div>
+			</div>
+			<!-- Card 3 -->
+			<div class="col">
+				<div class="card h-100">
+					<img src="https://via.placeholder.com/300x200.png?text=Produto+3" class="card-img-top" alt="Imagem do Produto 3">
+					<div class="card-body">
+						<h5 class="card-title">Produto 3</h5>
+						<p class="card-text">Descrição breve do produto 3.</p>
+					</div>
+					<div class="card-footer">
+						<small class="text-muted">$39.99</small>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="footer" class="fixed-bottom mt-5 pt-5 bg-light">
+
+		<footer id="footer">
+			<div class="footer-top">
+				<div class="container">
+					<div class="row text-muted">
+
+						<div class="col-lg-3 col-md-6">
+							<div class="footer-info">
+								<h3 class="text-dark">Malanga Store</h3>
+								<p>
+									Av.Nelson Mandela <br>
+									Zimpeto, Matendene<br><br>
+									<strong class="text-dark">Phone:</strong> +258 87 231 4523<br>
+									<strong class="text-dark">Email:</strong> info@malanga.com<br>
+								</p>
+								<div class="social-links mt-3">
+									<a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+									<a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+									<a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+									<a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+									<a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-lg-2 col-md-6 footer-links">
+							<h4 class="text-dark">Utilitarios</h4>
+							<ul>
+								<li><a href="#home">Início</a></li>
+								<li><a href="#parceiros">Parceiros</a></li>
+								<li><a href="#productos">Productos</a></li>
+								<li><a href="#sobre">Sobre nós</a></li>
+							</ul>
+						</div>
+
+						<div class="col-lg-3 col-md-6 footer-links">
+							<h4 class="text-dark">Nossos serviços</h4>
+							<ul>
+								<li><a href="#">E-commerce</a></li>
+								<li><a href="#">Cloud Computing</a></li>
+								<li><a href="#">Assistência tecnica</a></li>
+								<li><a href="#">Segurança de Informação</a></li>
+							</ul>
+						</div>
+
+						<div class="col-lg-4 col-md-6 footer-newsletter">
+							<h4 class="text-dark">Nossas Novidades</h4>
+							<p>Subscreva-se com o seu email para ser notificado quando novos productos forem adicionados</p>
+							<form action="#" method="post">
+								<div class="d-flex">
+									<input type="email" name="email" id="email" class="me-1 form-control hidden-focus">
+									<button class="btn btn-primary">Subscrever</button>
+								</div>
+							</form>
+
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</footer>
+	</section>
+</body>
+
+</html>
